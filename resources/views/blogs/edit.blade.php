61c39bcf0c9a134a40652d91b48a30be20586286
unknown <you@example.com>
2019-05-08 16:08:22 +0530

    first commit

---
+++ resources/views/blogs/edit.blade.php
@@ -0,0 +1,29 @@
+@extends('layouts.app')
+
+@section('content')
+
+<form action="{{ route('update_blog_path',['blog'=>$blog->id])}}" method="post">
+
+@csrf
+
+@method('PUT')
+
+<div class="form-group">
+<lable for="title"> Title</label>
+<input type="text" name="title" class="form-control" value="{{$blog->title}}">
+</div>
+
+
+<div class="form-group">
+<label for="content"> Content</label>
+</div>
+<textarea name="content" rows="10" class="form-control">{{$blog->content}}</textarea>
+
+
+<div class="form-group">
+<button type="submit" class="btn btn-outline-primary"> edit Blog Post</button>
+</div>
+
+
+</form>
+@endsection
---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>laravel</title>
+</head>
+<body>
+<nav class="navbar navbar-expand-lg navbar-sucess bg-dark">
+  <a class="navbar-brand" href="#">Navbar</a>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+
+  <div class="collapse navbar-collapse" id="navbarSupportedContent">
+    <ul class="navbar-nav ml-md-auto">
+      <li class="nav-item active">
+        <a class="nav-link" href="{{route('blogs_path')}}">Home <span class="sr-only">(current)</span></a>
+      </li>
+      
+    <li class="nav-item">
+    <a href="{{route('create_blog_path')}}" class="nav-link"> Create  Blog Post </a>
+    </li>
+
+
+  </div>
+
+</nav>
+
+<div class="container">
+
+    @yield('content')
+
+    </div>
+</body> 
+</html>
